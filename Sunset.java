acc447531a7de9aeea170c5dbcd8f1c1d81e9108
Ann Root <ann.root@bvsd.org>
2024-11-06 14:09:26 -0700

    Add files via upload

---
+++ Sunset.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class Sunset here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Sunset
+{
+    public static void main(String[] args)
+    {
+        int[] testArray1 = {3, 7, 8, 3, 6, 1};
+        int[] testArray2 = {3, 4, 6, 1, 7, 8, 5, 2};
+        int[] testArray3 = {7, 6, 5, 4,3, 2,1};
+        int[] testArray4 = {1, 3, 6, 8, 9, 10};
+        int[] testArray5 = {1, 2, 4, 5, 7, 1};
+        
+        
+        
+        
+    }
+}
